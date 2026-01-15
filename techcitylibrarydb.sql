e54e714ccbc425f8858c4e13eb96c5a9daeb054c
sahhoutiamine <midoriaduko@gmail.com>
2026-01-15 01:44:46 +0100

    create book table

--- techcitylibrarydb.sql
+++ techcitylibrarydb.sql
@@ -3,3 +3,6 @@ CREATE DATABASE techcitylibrarydb;
 CREATE TABLE Category (category_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100) NOT NULL);
 
 CREATE TABLE Author (author_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(200) NOT NULL, biography TEXT, nationality VARCHAR(100), birth_date DATE, primary_genre VARCHAR(100));
+
+CREATE TABLE Book (isbn VARCHAR(13) PRIMARY KEY, title VARCHAR(500) NOT NULL, publication_year INT, available_copies INT DEFAULT 0, status VARCHAR(50) DEFAULT 'Available', category_id INT, FOREIGN KEY (category_id) REFERENCES Category(category_id));
+
