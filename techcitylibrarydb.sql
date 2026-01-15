bf8384347fa55a572539050779b840d2a30e79a1
sahhoutiamine <midoriaduko@gmail.com>
2026-01-15 01:46:33 +0100

    create branch_inventory table

--- techcitylibrarydb.sql
+++ techcitylibrarydb.sql
@@ -11,3 +11,4 @@ CREATE TABLE Book_Author (book_isbn VARCHAR(13), author_id INT, PRIMARY KEY (boo
 CREATE TABLE Library_Branch (branch_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(200) NOT NULL, location VARCHAR(500), contact_number VARCHAR(20));
 
 
+CREATE TABLE Branch_Inventory (branch_id INT, book_isbn VARCHAR(13), copies INT DEFAULT 0, PRIMARY KEY (branch_id, book_isbn), FOREIGN KEY (branch_id) REFERENCES Library_Branch(branch_id), FOREIGN KEY (book_isbn) REFERENCES Book(isbn));
