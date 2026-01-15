57d7607f0eb5a6a85fcda749adb13bd4a6c1b3f6
sahhoutiamine <midoriaduko@gmail.com>
2026-01-15 01:44:20 +0100

    create author table

--- techcitylibrarydb.sql
+++ techcitylibrarydb.sql
@@ -1,3 +1,5 @@
 CREATE DATABASE techcitylibrarydb;
 
 CREATE TABLE Category (category_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100) NOT NULL);
+
+CREATE TABLE Author (author_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(200) NOT NULL, biography TEXT, nationality VARCHAR(100), birth_date DATE, primary_genre VARCHAR(100));
